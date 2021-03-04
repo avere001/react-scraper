4dd140cd24ca78b3e6b776f49932eb1bc65c54d1
Adam V <adam@example.com>
2021-03-03 18:30:39 -0800

    Finish the API

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import httpx
 from flask import Flask
 from flask_restx import Resource, Api, reqparse, inputs, fields
 
@@ -9,11 +10,8 @@ app.config['RESTX_MASK_SWAGGER'] = False
 
 website_response_model = api.model('WebsiteResponse', {
     'content': fields.String,
-    'url': fields.Url
+    'url': fields.String,
 })
-
-website_request_parser = reqparse.RequestParser()
-website_request_parser.add_argument('url', type=inputs.url, required=True)
 
 
 class WebsiteResponseData(object):
@@ -22,19 +20,41 @@ class WebsiteResponseData(object):
         self.content = content
 
 
+website_request_parser = reqparse.RequestParser()
+website_request_parser.add_argument('url', type=inputs.url, required=True)
+
+httpx_request_error_model = api.model(
+    'OutgoingRequestError',
+    {'url': fields.String, 'message': fields.String, 'details': fields.String},
+)
+
+
+@api.errorhandler(httpx.RequestError)
+@api.marshal_with(httpx_request_error_model, code=500)
+def handle_httpx_request_error(error):
+    return {'message': 'An error occurred while requesting the given URL.',
+            'url': error.request.url,
+            'details': '\n'.join(error.args)}
+
+
 @api.route('/website')
 class Website(Resource):
 
     @api.marshal_with(website_response_model)
     @api.expect(website_request_parser, validate=True)
+    @api.response(500, description='The request to the given URL failed', model=httpx_request_error_model)
     def get(self):
+        """
+        Get the HTML of the given website.
+        """
         args = website_request_parser.parse_args()
-        return WebsiteResponseData(
-            url=args['url'], content='<h1>WOW</h1>')
+        url = args['url']
+
+        response = httpx.get(url)
+        return WebsiteResponseData(url=url, content=response.text)
 
 
 api.add_resource(Website, '/website')
-
 
 if __name__ == '__main__':
     app.run()
